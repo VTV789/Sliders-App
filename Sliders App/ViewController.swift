e7ac5d73d7d975af24c3bbeb3e6616631777eeb8
Vinh Thanh Vu <vtv@vinh.com>
2015-09-10 23:45:59 -0400

    Add changeColorComponent to viewDidLoad
    colorView.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1)

--- Sliders App/ViewController.swift
+++ Sliders App/ViewController.swift
@@ -19,6 +19,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        changeColorComponent()
        
     }
     
@@ -26,6 +27,8 @@ class ViewController: UIViewController {
         let r: CGFloat = CGFloat(self.redControl.value)
         let g: CGFloat = CGFloat(self.greenControl.value)
         let b: CGFloat = CGFloat(self.blueControl.value)
+        
+        colorView.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1)
     }
 }
 
