6d1fa586b4d76e5133b33bcbefae5554312629f5
Vinh Thanh Vu <vtv@vinh.com>
2015-09-10 23:37:31 -0400

    Connect the three Outlets and Action

--- Sliders App/ViewController.swift
+++ Sliders App/ViewController.swift
@@ -13,18 +13,19 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var colorView: UIView!
     
-    @IBOutlet weak var redControll: UISlider!
-    @IBOutlet weak var greenController: UISlider!
-    @IBOutlet weak var blueController: UISlider!
-    
+    @IBOutlet weak var redControl: UISlider!
+    @IBOutlet weak var greenControl: UISlider!
+    @IBOutlet weak var blueControl: UISlider!
     
     override func viewDidLoad() {
         super.viewDidLoad()
        
     }
-
     
-
-
+    @IBAction func changeColorComponent() {
+        let r: CGFloat = CGFloat(self.redControl.value)
+        let g: CGFloat = CGFloat(self.greenControl.value)
+        let b: CGFloat = CGFloat(self.blueControl.value)
+    }
 }
 
